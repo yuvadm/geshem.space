190a16ee4a38e2a68c75daebd43126fd2f62a936
Yuval Adam <_@yuv.al>
2019-10-16 22:29:59 +0300

    Fix warnings

--- src/Geshem.js
+++ src/Geshem.js
@@ -51,7 +51,7 @@ function Geshem(props) {
 
     if (playback) buildPlayback();
     else fetchImages();
-  }, []);
+  }, [playback]);
 
   return (
     <>
--- src/Slider.js
+++ src/Slider.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, Component } from "react";
+import React from "react";
 import RcSlider from "rc-slider";
 
 function Slider(props) {
