9a49d5b71df9a7a77a3cbf9d13f1468af11f66ed
Yuval Adam <_@yuv.al>
2019-11-01 08:25:55 +0200

    Formatting

--- src/Geshem.js
+++ src/Geshem.js
@@ -27,19 +27,19 @@ function Geshem(props) {
   const [slider, setSlider] = useState(playback ? 143 : 9);
 
   useEffect(() => {
-    const fetchImages = async () => 
+    const fetchImages = async () =>
       fetch(`${IMAGES_BASE_URL}/imgs.json`)
         .then(res => res.json())
         .then(imgs => imgs['280'])
         .then(setImages)
     )
-    
+
     const buildPlayback = async () => setImages(
       Array(24*6)
         .fill('')
         .map((_,i) => `imgs/${playback}/${`${parseInt(i/6, 10)%24}`.padStart(2, '0')}${i%6}0/280.png`)
     )
-    
+
     return playback ?
       (buildPlayback(), () => void 0) :
       (fetchImages(), ((timer) => () => clearTimer(timer))(setInterval(fetchImages, 60 * 1000))
