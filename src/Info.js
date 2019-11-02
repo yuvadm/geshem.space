bbed70b0f4415ef337de391e21805a0d3ecab635
Yuval Adam <_@yuv.al>
2019-11-02 23:08:11 +0200

    Add hebrew and english placeholder copy

--- src/Info.js
+++ src/Info.js
@@ -14,7 +14,24 @@ function Info(props) {
         ?
       </div>
       <div id="info-modal" style={{ display: open ? "block" : "none" }}>
-        This is the info page
+        <div id="info-eng">
+          <p>Geshem.space shows real-time rain radar data in Israel.</p>
+          <p>The entire source code is open and available at</p>
+          <p>
+            <a href="https://github.com/yuvadm/geshem.space">
+              https://github.com/yuvadm/geshem.space
+            </a>
+          </p>
+        </div>
+        <div id="info-heb">
+          <p>גשם.ספייס מציג מידע בזמן אמת על ענני גשם בכל הארץ</p>
+          <p>קוד המקור של האתר פתוח וזמין לצפייה בכתובת</p>
+          <p>
+            <a href="https://github.com/yuvadm/geshem.space">
+              https://github.com/yuvadm/geshem.space
+            </a>
+          </p>
+        </div>
       </div>
     </div>
   );
