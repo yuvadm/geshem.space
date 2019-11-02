53611b9fff09992e73755112492f699ae19a8805
Yuval Adam <_@yuv.al>
2019-11-02 22:58:53 +0200

    Info modal skeleton

--- src/Info.js
+++ src/Info.js
@@ -1,22 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
 
 function Info(props) {
   const [open, setOpen] = useState(false);
 
-  if (images.length > 0) {
-    const ds = images[slider].substr(5, 13);
-    datetime = LuxonDateTime.fromFormat(ds, "yyyyMMdd/HHmm", {
-      zone: "utc"
-    }).setZone("Asia/Jerusalem");
-  }
-
-  const date = datetime ? datetime.toFormat("dd-MM-y") : "";
-  const time = datetime ? datetime.toFormat("HH:mm") : "";
-
   return (
     <div id="info">
-      <div id="info-button">?</div>
-      <div id="info-modal">This is the info page</div>
+      <div
+        id="info-button"
+        onClick={() => {
+          setOpen(!open);
+        }}
+      >
+        ?
+      </div>
+      <div id="info-modal" style={{ display: open ? "block" : "none" }}>
+        This is the info page
+      </div>
     </div>
   );
 }
