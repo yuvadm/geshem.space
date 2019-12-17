4a322d3faa8b64dcca93849a5f7ebe7970ddd43e
Yuval Adam <_@yuv.al>
2019-12-17 14:27:17 +0200

    Info WIP

--- src/Info.js
+++ src/Info.js
@@ -15,21 +15,45 @@ function Info(props) {
       </div>
       <div id="info-modal" style={{ display: open ? "block" : "none" }}>
         <div id="info-eng">
-          <p>Geshem.space shows real-time rain radar data in Israel.</p>
-          <p>The entire source code is open and available at</p>
           <p>
-            <a href="https://github.com/yuvadm/geshem.space">
-              https://github.com/yuvadm/geshem.space
-            </a>
+            <strong>
+              Geshem.space shows real-time rain radar data in Israel
+            </strong>
+          </p>
+          <p>
+            Rain clouds show up as colored patches on the map, a warmer color
+            means stronger percipication
+          </p>
+          <p>
+            Use the slider on the bottom to scroll through the animation of the
+            last hour and determine whether the clouds are coming your way
+          </p>
+          <p>
+            Developed by <a href="https://yuv.al">Yuval Adam</a>
+          </p>
+          <p>
+            Source code available on{" "}
+            <a href="https://github.com/yuvadm/geshem.space">Github</a>
           </p>
         </div>
         <div id="info-heb">
-          <p>גשם.ספייס מציג מידע בזמן אמת על ענני גשם בכל הארץ</p>
-          <p>קוד המקור של האתר פתוח וזמין לצפייה בכתובת</p>
           <p>
-            <a href="https://github.com/yuvadm/geshem.space">
-              https://github.com/yuvadm/geshem.space
-            </a>
+            <strong>גשם.ספייס מציג בזמן אמת ענני גשם בכל הארץ</strong>
+          </p>
+          <p>
+            עננים מופיעים כגושים צבעוניים על גבי המפה, צבע חם יותר מסמל עננות
+            צפופה ולכן גשם חזק יותר
+          </p>
+          <p>
+            השתמשו בסליידר בתחתית המסך כדי לגלול באנימציה של העננים בשעה האחרונה
+            על מנת להבין האם ענני הגשם בדרך אליכם
+          </p>
+          <p>
+            פותח ע"י <a href="https://yuv.al">יובל אדם</a>
+          </p>
+          <p>
+            קוד המקור זמין ב
+            <a href="https://github.com/yuvadm/geshem.space">גיטהאב</a>
           </p>
         </div>
       </div>
