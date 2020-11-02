6c62ed06df2c667b6d57094ee0c0af5bc0f283da
Yuval Adam <_@yuv.al>
2020-11-02 17:52:10 +0200

    DateTime styling improvements

--- src/Datetime.js
+++ src/Datetime.js
@@ -8,11 +8,11 @@ function DateTime(props) {
   if (images.length > 0) {
     const ds = images[slider].substr(5, 13);
     datetime = LuxonDateTime.fromFormat(ds, "yyyyMMdd/HHmm", {
-      zone: "utc"
+      zone: "utc",
     }).setZone("Asia/Jerusalem");
   }
 
-  const date = datetime ? datetime.toFormat("dd-MM-y") : "";
+  const date = datetime ? datetime.toFormat("dd/MM/y") : "";
   const time = datetime ? datetime.toFormat("HH:mm") : "";
 
   return (
