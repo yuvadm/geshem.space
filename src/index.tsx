44a1c5b7e62e83af4d670e011f5998b9b8aedfef
Yuval Adam <_@yuv.al>
2022-08-29 10:24:51 +0300

    Update all .tsx files and remove unused

--- src/index.tsx
+++ src/index.tsx
@@ -5,10 +5,9 @@ import App from './Geshem';
 
 console.log()
 
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
     <React.StrictMode>
         <App />
-    </React.StrictMode>
+    </React.StrictMode >
 );
